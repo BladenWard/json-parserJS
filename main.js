55917c441b50a6f0ce41e60283caa1594d1c9af6
Bladen Ward <Bladenward1213@gmail.com>
2024-05-21 15:14:30 -0500

    main() + rename

--- main.js
+++ main.js
@@ -196,28 +196,31 @@ function parseJson(json) {
     return json;
 }
 
-const args = process.argv.slice(2);
+function main() {
+    const args = process.argv.slice(2);
 
-let json;
-if (args.length === 0) {
-    console.error('No input provided. Please provide a JSON string or use the -f flag to specify a file.');
-    process.exit(1);
-} else if (args[0] === '-f') {
-    if (args.length < 2) {
-        console.error('Please provide a filename after the -f flag.');
+    let json;
+    if (args.length === 0) {
+        console.error('No input provided. Please provide a JSON string or use the -f flag to specify a file.');
         process.exit(1);
+    } else if (args[0] === '-f') {
+        if (args.length < 2) {
+            console.error('Please provide a filename after the -f flag.');
+            process.exit(1);
+        }
+
+        const filename = args[1];
+        try {
+            json = fs.readFileSync(filename, 'utf8');
+        } catch (err) {
+            console.error(`Error reading file ${filename}: ${err.message}`);
+            process.exit(1);
+        }
+    } else {
+        json = args.join(' ');
     }
 
-    const filename = args[1];
-    try {
-        json = fs.readFileSync(filename, 'utf8');
-    } catch (err) {
-        console.error(`Error reading file ${filename}: ${err.message}`);
-        process.exit(1);
-    }
-} else {
-    json = args.join(' ');
+    console.log("JSON:", parseJson(json));
 }
 
-const jsonData1 = parseJson('{ "name": "John", "age": 30 }');
-console.log("JSON:", json);
+main();
