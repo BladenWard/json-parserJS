48a5e7d7205243f1a92340eb9ceca556cc8c6a6c
Bladen Ward <Bladenward1213@gmail.com>
2024-05-21 14:46:54 -0500

    parsing package.json(s)

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function parseJson(json) {
     if (typeof json != "string") {
         throw new Error('Input is not a string');
@@ -194,5 +196,28 @@ function parseJson(json) {
     return json;
 }
 
+const args = process.argv.slice(2);
+
+let json;
+if (args.length === 0) {
+    console.error('No input provided. Please provide a JSON string or use the -f flag to specify a file.');
+    process.exit(1);
+} else if (args[0] === '-f') {
+    if (args.length < 2) {
+        console.error('Please provide a filename after the -f flag.');
+        process.exit(1);
+    }
+
+    const filename = args[1];
+    try {
+        json = fs.readFileSync(filename, 'utf8');
+    } catch (err) {
+        console.error(`Error reading file ${filename}: ${err.message}`);
+        process.exit(1);
+    }
+} else {
+    json = args.join(' ');
+}
+
 const jsonData1 = parseJson('{ "name": "John", "age": 30 }');
-console.log("JSON:", jsonData1);
+console.log("JSON:", json);
